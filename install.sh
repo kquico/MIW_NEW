7f890c951efd1e5292fa2134a5ccc1ee13e6b694
Jose Francisco <kquico@gmail.com>
2014-12-31 15:56:43 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+export DEBIAN_FRONTEND=noninteractive
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password PASS'
+sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password PASS'
+sudo apt-get install -y mariadb-server
