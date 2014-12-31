99c33d798239332606088d99dee4e5d303ea9d24
Jose Francisco <kquico@gmail.com>
2014-12-31 17:11:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Inicio instalacion mariadb"
 export DEBIAN_FRONTEND=noninteractive
 sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password password PASS'
 sudo debconf-set-selections <<< 'mariadb-server-10.0 mysql-server/root_password_again password PASS'
 sudo apt-get install -y mariadb-server
+echo "Fin instalacion mariadb"
